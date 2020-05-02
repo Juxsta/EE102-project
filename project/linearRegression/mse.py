dc753fbae02253a7070382bd6e47e62a2004ea83
Eric Reyes <ericjrreyes2@gmail.com>
2020-05-02 12:48:36 -0700

    :construction: mean-squared-error

--- project/linearRegression/mse.py
+++ project/linearRegression/mse.py
@@ -1,3 +1,4 @@
 import numpy as np
-def mse(x:np.ndarray,y:np.ndarray):
-    return x-y
+from sklearn.linear_model.base import LinearRegression
+def mse(model:LinearRegression,x:np.ndarray,y:np.ndarray):
+    model.predict()
